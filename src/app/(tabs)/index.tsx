819ed1cb4fcb51f52fe1db592d4517637fff2f6b
mfgcorpusjr <mcorpus.dev@gmail.com>
2025-07-31 22:44:44 +0800

    home screen keyboard avoiding view

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,4 +1,10 @@
-import { FlatList, ActivityIndicator, RefreshControl } from "react-native";
+import {
+  FlatList,
+  ActivityIndicator,
+  RefreshControl,
+  KeyboardAvoidingView,
+  Platform,
+} from "react-native";
 
 import Wrapper from "@/components/common/Wrapper";
 import HomeHeader from "@/components/home/HomeHeader";
@@ -23,34 +29,41 @@ export default function HomeScreen() {
 
   return (
     <Wrapper>
-      <FlatList
-        data={data?.pages.flat() || []}
-        keyExtractor={(item) => item.id.toString()}
-        renderItem={({ item }) => <TodoListItem todo={item} />}
-        refreshControl={
-          <RefreshControl
-            refreshing={isRefetching}
-            onRefresh={refetch}
-            tintColor={colors.text}
-          />
-        }
-        onEndReached={() =>
-          !isFetchingNextPage && hasNextPage && fetchNextPage()
-        }
-        ListHeaderComponent={() => <HomeHeader />}
-        ListFooterComponent={
-          isFetchingNextPage ? <ActivityIndicator color={colors.text} /> : null
-        }
-        ListEmptyComponent={() => (
-          <ListEmpty
-            isLoading={isLoading}
-            title="No todos yet"
-            subTitle="Add your first todo above to get started"
-          />
-        )}
-        showsVerticalScrollIndicator={false}
-        contentContainerStyle={{ gap: 16, padding: 16 }}
-      />
+      <KeyboardAvoidingView
+        behavior={Platform.OS === "ios" ? "padding" : "height"}
+        style={{ flex: 1 }}
+      >
+        <FlatList
+          data={data?.pages.flat() || []}
+          keyExtractor={(item) => item.id.toString()}
+          renderItem={({ item }) => <TodoListItem todo={item} />}
+          refreshControl={
+            <RefreshControl
+              refreshing={isRefetching}
+              onRefresh={refetch}
+              tintColor={colors.text}
+            />
+          }
+          onEndReached={() =>
+            !isFetchingNextPage && hasNextPage && fetchNextPage()
+          }
+          ListHeaderComponent={() => <HomeHeader />}
+          ListFooterComponent={
+            isFetchingNextPage ? (
+              <ActivityIndicator color={colors.text} />
+            ) : null
+          }
+          ListEmptyComponent={() => (
+            <ListEmpty
+              isLoading={isLoading}
+              title="No todos yet"
+              subTitle="Add your first todo above to get started"
+            />
+          )}
+          showsVerticalScrollIndicator={false}
+          contentContainerStyle={{ gap: 16, padding: 16 }}
+        />
+      </KeyboardAvoidingView>
     </Wrapper>
   );
 }
