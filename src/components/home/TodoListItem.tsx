a0f845c54bb6a7937b750b8098db187a72295df6
mfgcorpusjr <mcorpus.dev@gmail.com>
2025-07-31 09:35:37 +0800

    todo list item ui

--- src/components/home/TodoListItem.tsx
+++ src/components/home/TodoListItem.tsx
@@ -1,6 +1,8 @@
-import { View, Text } from "react-native";
+import { StyleSheet, View, Text } from "react-native";
+import Ionicons from "@expo/vector-icons/Ionicons";
 
 import Card from "@/components/common/Card";
+import IconButton from "../ui/IconButton";
 
 import { Tables } from "@/types/database.types";
 import { useThemeContext } from "@/providers/ThemeProvider";
@@ -14,7 +16,48 @@ export default function TodoListItem({ todo }: TodoListItemProps) {
 
   return (
     <Card>
-      <Text style={{ color: colors.text }}>{todo.text}</Text>
+      <View style={styles.container}>
+        <Ionicons
+          name={todo.is_completed ? "checkmark-circle" : "pause-circle"}
+          size={38}
+          color={todo.is_completed ? colors.success : colors.textMuted}
+        />
+
+        <View style={{ gap: 16 }}>
+          <Text style={[styles.text, { color: colors.text }]}>{todo.text}</Text>
+
+          <View style={styles.actionsContainer}>
+            <IconButton
+              icon={<Ionicons name="pencil" size={18} color="white" />}
+              size="sm"
+              type="warning"
+              rounded
+            />
+            <IconButton
+              icon={<Ionicons name="trash" size={18} color="white" />}
+              size="sm"
+              type="danger"
+              rounded
+            />
+          </View>
+        </View>
+      </View>
     </Card>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: "row",
+    gap: 16,
+  },
+  text: {
+    fontSize: 16,
+    fontWeight: "600",
+  },
+  actionsContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    gap: 12,
+  },
+});
